03b80e6e39a50710dd839c9ecf468104d0278ad4
Tsh3py <1tshepi.mohitshane@gmail.com>
2025-07-13 13:36:47 +0200

    feat: Create and integrate WelcomeMessage component, modify JSX

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,14 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+// NEW: Import your WelcomeMessage component
+import WelcomeMessage from './components/WelcomeMessage'; 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      {/* RENDER THE WelcomeMessage COMPONENT HERE */}
+      <WelcomeMessage /> 
+
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
