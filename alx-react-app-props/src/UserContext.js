b544cb1e59f4b6575a8d79cb553f52af3e1c45b8
Tsh3py <1tshepi.mohitshane@gmail.com>
2025-07-16 14:21:14 +0200

    feat:fix UserContext.js

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,8 +1,6 @@
-import React from 'react';
+ import React from 'react'; 
 
-// Initialize a Context and export it.
-// It's common practice to provide a default value, often null or an empty object,
-// though for this specific use case, it's primarily the creation that matters.
-const UserContext = React.createContext(null); 
+        const UserContext = React.createContext(null); 
 
-export default UserContext;
+        export default UserContext;
+        
