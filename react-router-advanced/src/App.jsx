13b0143a50b07b22df54d0a9eea5b1b05830b476
Tsh3py <1tshepi.mohitshane@gmail.com>
2025-08-19 14:54:45 +0200

    feat:Protected route implemented

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,32 +1,52 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './components/Home';
+import About from './components/About';
+import BlogPosts from './components/BlogPosts';
 import Profile from './components/Profile';
-import Blog from './components/BlogPosts'; 
+import ProtectedRoute from './components/ProtectedRoute'; // Import your ProtectedRoute component
+import Login from './components/Login';
+import { AuthProvider } from './AuthContext';
+
 function App() {
   return (
     <Router>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/profile">Profile</Link>
-          </li>
-          <li>
-            <Link to="/blog">Blog</Link>
-          </li>
-        </ul>
-      </nav>
+      <AuthProvider>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/posts">Blog Posts</Link>
+            </li>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav>
 
-      <hr />
+        <hr />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/profile/*" element={<Profile />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/blog/:id" element={<Blog />} /> 
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/posts" element={<BlogPosts />} />
+          <Route path="/posts/:postId" element={<BlogPosts />} />
+          
+          {/* Use ProtectedRoute to protect the Profile route */}
+          <Route path="/profile/*" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+          
+          <Route path="/login" element={<Login />} />
+          
+          <Route path="*" element={<h1>Page Not Found</h1>} />
+        </Routes>
+      </AuthProvider>
     </Router>
   );
 }
