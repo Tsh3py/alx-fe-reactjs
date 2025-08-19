05868169a86810b5154b8d465d9e8c759063a1de
Tsh3py <1tshepi.mohitshane@gmail.com>
2025-08-19 14:35:43 +0200

    feat:Advanced Routing in React with React Router

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,52 +1,32 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './components/Home';
-import About from './components/About';
-import BlogPosts from './components/BlogPosts';
 import Profile from './components/Profile';
-import PrivateRoute from './components/PrivateRoute';
-import Login from './components/Login';
-import { AuthProvider } from './AuthContext'; // We'll create this in a bit
-
+import Blog from './components/Blog'; 
 function App() {
   return (
     <Router>
-      <AuthProvider>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/posts">Blog Posts</Link>
-            </li>
-            <li>
-              <Link to="/profile">Profile</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-          </ul>
-        </nav>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
+          <li>
+            <Link to="/blog">Blog</Link>
+          </li>
+        </ul>
+      </nav>
 
-        <hr />
+      <hr />
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/posts" element={<BlogPosts />} />
-          <Route path="/posts/:postId" element={<BlogPosts />} />
-          
-          <Route path="/profile/*" element={<PrivateRoute><Profile /></PrivateRoute>} />
-          
-          <Route path="/login" element={<Login />} />
-          
-          {/* A catch-all route for any undefined paths */}
-          <Route path="*" element={<h1>Page Not Found</h1>} />
-        </Routes>
-      </AuthProvider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile/*" element={<Profile />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/blog/:id" element={<Blog />} /> 
+      </Routes>
     </Router>
   );
 }
